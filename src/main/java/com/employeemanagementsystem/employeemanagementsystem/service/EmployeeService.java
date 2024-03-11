99f0a1a91c86dc978a41d1028ae0f6770637426e
hvferreira <heldervferreira@gmail.com>
2024-03-11 18:35:34 +0000

    create

---
+++ src/main/java/com/employeemanagementsystem/employeemanagementsystem/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.employeemanagementsystem.employeemanagementsystem.service;
+
+public class EmployeeService {
+}
---
+++ src/test/java/com/employeemanagementsystem/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagementsystem.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
